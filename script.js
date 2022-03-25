57e808786629d8946cc6dcbadc54390a325fa359
Saad Niazi <saadniazifed@gmail.com>
2022-03-25 13:52:52 +0500

    Finished with the Step 4 of adding the majority of the functionality as well as the CSS. Every module is working as expected.

--- script.js
+++ script.js
@@ -1,32 +1,111 @@
-const Game = (function () {
-  //
-  //   const playerOne = playerFactory("Slim", "O");
-  //   const playerTwo = playerFactory("Shady", "X");
-})();
+const playerFactory = (name, marker) => {
+  const getName = name;
+  const getMarker = marker;
 
-const playerFactory = (name, choice) => {
-  //players get created here.
+  let _playerMove = 0;
+
+  function _playerMoveCount() {
+    return _playerMove++;
+  }
+
+  function playerMoveCount() {
+    _playerMoveCount();
+  }
+
+  return { getName, getMarker, playerMoveCount };
 };
 
-const gameBoard = (function () {
-  const unique = document.querySelector("#unique");
+const Game = (function () {
+  //A module that plays out entire game of Tic Tac Toe.
 
-  const gameBoardObj = {
-    gameBoard: ["X", "O", "X", "X", "O", "X", "X", "O", "X"],
+  return {};
+})();
+
+//Module responsible for the Tic Tac Toe Gameboard.
+const gameBoard = (function () {
+  const gameboardArray = ["", "", "", "", "", "", "", "", ""];
+
+  function setMark(index, playerMarker) {
+    gameboardArray[index] = playerMarker;
+  }
+
+  return {
+    setMark,
+  };
+})();
+
+//Module Responsible for the UI Change in the game.
+const displayController = (function () {
+  //Selecting the DOM Elements
+  const _squares = document.querySelectorAll(".square");
+
+  const addMarkersOnScreen = function () {
+    _squares.forEach((square) => {
+      square.addEventListener("click", () => {
+        square.textContent = gameController.returnMarker();
+        square.classList.add("markerStyling");
+
+        if (square.classList.contains("markerStyling")) {
+          square.classList.add("avoid-clicks");
+        }
+      });
+    });
+  };
+  return {
+    addMarkersOnScreen,
+  };
+})();
+
+//Module responsible for the flow of the game.
+const gameController = (function () {
+  //Selecting the DOM Elements
+  const _squares = document.querySelectorAll(".square");
+
+  //Creating Objects.
+  const playerOne = playerFactory("Slim", "X");
+  const playerTwo = playerFactory("Shady", "O");
+
+  //Event Handler function that invokes getIndex and returnMarker function.
+  function addEventHandler() {
+    _squares.forEach((square) => {
+      square.addEventListener("click", (e) => {
+        getIndex(e);
+        returnMarker(e);
+      });
+    });
+  }
+
+  const getIndex = (e) => {
+    const dataIndex = e.target.dataset.index;
+    gameBoard.setMark(dataIndex, returnMarker);
   };
 
-  const displayArray = function () {
-    for (let i = 0; i < gameBoardObj.gameBoard.length; i++) {
-      unique.textContent = gameBoardObj.gameBoard.join(", ");
+  let totalMoves = 0;
+
+  const returnMarker = () => {
+    if (totalMoves % 2 != 0) {
+      return getMarkX();
+    } else if (totalMoves % 2 == 0) {
+      return getMarkO();
     }
   };
 
-  //   displayArray();
+  function getMarkO() {
+    totalMoves++;
+    playerTwo.playerMoveCount();
+    return "O";
+  }
 
-  return {};
+  function getMarkX() {
+    totalMoves++;
+    playerOne.playerMoveCount();
+    return "X";
+  }
+
+  return {
+    addEventHandler,
+    returnMarker,
+  };
 })();
 
-const displayController = (function () {
-  //Responsible for the UI Change
-  return {};
-})();
+displayController.addMarkersOnScreen();
